a01a0e640499d4247543f745d522f4b39703111e
Prashant <maskeprashant.07@gmail.com>
2026-01-30 12:20:00 +0530

    python string methods

---
+++ day4/str_methods.py
@@ -0,0 +1,34 @@
+# String methods in Python
+# conctnation
+
+greeting = "Hello"
+name = "Alice"
+
+message = greeting + " " + name + "!"
+print(message)  # Output: Hello Alice!
+
+# upper() and lower() methods
+print(greeting.upper())  # Output: HELLO
+print(name.lower())      # Output: alice
+
+# slicing
+message = "Hello, world!"
+        #  01234567890123
+
+print(message[0:5])  # Output: Hello
+print(message[7:12]) # Output: world
+
+# find method
+
+index = message.find("world")
+print(f"'world' found at index: {index}")  # Output: 'world'
+
+str = "this is wonderful world"
+print(str.find("wonderful"))
+
+for i in str:
+    print(i)
+
+str_ar = str.split(" ")
+for st in str_ar:
+    print(st)
